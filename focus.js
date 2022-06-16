5b6bcaacd6b0d45eddce701698919f91edfcf4dd
Nicholas Amheiser <91265137+Amheisern@users.noreply.github.com>
2022-06-15 20:35:42 -0700

    interview question follow ups

--- focus.js
+++ focus.js
@@ -3,28 +3,30 @@
 // The getter can return a string of foo. Then, use the newly created class to call the getField method
 
 class Main {
+
   get field() {
     return 'foo';
   }
   getField() {
     return this.field;
   }
-  
 }
+
 // 2) Using WebdriverIO, assume there is a page with an input like <input data-field="score" />.
 //  How would you set a value of 123 in the input?
 //  Then, after you set the value how would you get the value of that same input?
 // $(selector).setValue(value, { translateToUnicode })
-<input data-field="score" />
-// v4
-browser.setValue('input[data-field="score"]', 123);
+
+<input data-field="score"/>
 // v7
 // Would eb addValue() if element doesn't need to be cleared 
-$('input[data-field="score"]').setValue(123);
+ $('input[data-field="score"]').setValue(123);
+ $('input[data-field="score"]').getValue();
+
 
 // v4  
 browser.getValue('input[data-field="score"]');
-$('input[data-field="score"]').getValue();
+browser.setValue('input[data-field="score"]', 123);
 
 
 // 3) How would you select the text from the following element
